45a683e44b4cad5f259944ab10cd016eae69e593
David Silva <d.silvas@outlook.com>
2024-08-22 08:41:00 +0100

    Hide Headers and Adjust Indentation in HTML Posts
    Commented out the headers in multiple HTML files to suppress their display. Adjusted indentation in these files and others for improved readability and uniformity. Added custom styling rules and new database entries for posts.

---
+++ migrations/src/main/resources/db/migration/V1_202408220830__add-more-posts.sql
@@ -0,0 +1,11 @@
+insert into posts (created, published, location, title, category_name)
+values ('2024-06-10 17:45:00',
+        '2024-06-10 17:45:00',
+        'posts/09.html',
+        'More cool music',
+        'Random'),
+       ('2024-06-20 17:55:00',
+        '2024-06-20 17:55:00',
+        'posts/10.html',
+        'Selected WebDev topics',
+        'Programming');
